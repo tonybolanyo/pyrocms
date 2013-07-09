0b7a442c41511fc42293f04d3175a2ae15c72970
tonybolanyo <tonybolanyo@gmail.com>
2013-07-09 09:18:02 +0200

    'contact_submit_error' translated to spanish

--- system/cms/modules/contact/language/spanish/contact_lang.php
+++ system/cms/modules/contact/language/spanish/contact_lang.php
@@ -1,5 +1,5 @@
 <?php defined('BASEPATH') or exit('No direct script access allowed');
 
-$lang['contact_submit_error']  = 'Sorry. We have concluded that you may be a bot. Please accept our apologies if we are in error and contact us using some other means.'; #translate
+$lang['contact_submit_error']  = 'Lamentablemente hemos llegado a la conclusión de que eres un bot. Acepta nuestras disculpas si es un error y contacta con nosotros a través de otro medio.';
 $lang['contact_sent_text']     = 'Tu mensaje ha sido enviado. Te responderemos tan pronto podamos.';
 $lang['contact_error_message'] = 'Hubo un problema enviando el mensaje. Por favor intente nuevamente más tarde.';
